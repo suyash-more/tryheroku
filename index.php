22614aa709a996031b280716f240fcc75790cacf
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-05-23 10:31:26 +0530

    frame added"

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $name="<h1>Yeh hai bora ka bday</h1>";
 </head>
 <body style="height:100vh;">
     <iframe width=100% height=100% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" 
-    frameborder="0" allowfullscreen>
+    frameborder="0" allowfullscreen></iframe>
     </iframe>
     </body>
 </html>
