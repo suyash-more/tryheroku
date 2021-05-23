38b5b8aea7bf187f1defad3d665a6f64bb89c72b
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-05-23 11:39:04 +0530

    final changes done

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 </head>
 
 <body style="height:100vh;">
-    <iframe width=100% height=100% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" frameborder="0" allowfullscreen></iframe>
-    </iframe>
+    <iframe width=100% height=98% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" 
+    frameborder="0" allowfullscreen></iframe>
 </body>
 
 </html>
