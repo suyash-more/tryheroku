0bcd5718f8a6b4990d76e2bf36447adb35edd5b8
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-05-23 10:41:16 +0530

    favicon added"

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Happy BirthDay Baccho &#x1F606;</title>
+    <link rel="shortcut icon" type="image/jpg" href="final.jpg"/>
 </head>
 
 <body style="height:100vh;">
