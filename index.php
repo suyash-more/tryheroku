0e6657b0d4a8cc4c26404549fc0e9c93c91e6463
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-05-23 10:38:28 +0530

    emoji added

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-$name="<h1>Yeh hai bora ka bday</h1>";
 
 ?>
 <!DOCTYPE html>
@@ -8,7 +7,7 @@ $name="<h1>Yeh hai bora ka bday</h1>";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Happy BirthDay Baccho &#x1F606;</title>
 </head>
 <body style="height:100vh;">
     <iframe width=100% height=100% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" 
