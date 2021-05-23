6208fd4f5d8f589a042737bcc45901f21f888bee
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-05-23 10:39:12 +0530

    formatting done so far

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Happy BirthDay Baccho &#x1F606;</title>
 </head>
+
 <body style="height:100vh;">
-    <iframe width=100% height=100% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" 
-    frameborder="0" allowfullscreen></iframe>
+    <iframe width=100% height=100% src="https://www.artsteps.com/embed/60a4a14c96724cf154c6df2f/560/315" frameborder="0" allowfullscreen></iframe>
     </iframe>
-    </body>
+</body>
+
 </html>
